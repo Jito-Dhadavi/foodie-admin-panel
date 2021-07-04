4a69c650ebf18931f90ff81a192ea7511eb3a56f
Jito-Dhadavi <dhadavijitendra999@gmail.com>
2021-07-04 23:36:14 +0530

    deploy fixing

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl:"https://food-order-system-backend.herokuapp.com/api/",
-  baseUrl:'https://food-order-system-backend.herokuapp.com/'
-
-};
